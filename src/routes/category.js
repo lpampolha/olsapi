1fefb44a1eddfd550a18cb002c2c0a58c85fc7ac
Lucas Pampolha <lucaspampolhadasilva@gmail.com>
2020-12-31 19:13:25 -0300

    Importação de Messages

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const Auth = require('../middlewares/auth')
+const file = require('../middlewares/file')
 const MSGS = require('../../messages')
 const Category = require('../models/category')
 
@@ -34,5 +36,27 @@ router.get('/:userId', async (req, res) => {
         res.status(500).send({ "error": MSGS.GENERIC_ERROR })
     }
 })
+
+// router.post('/', file, async (req, res, next) => {
+//     try {
+//         const errors = validationResult(req)
+//         if (!errors.isEmpty()) {
+//             return res.status(400).json({ errors: errors.array() })
+//         } else {
+//             req.body = `category/${req.body}`
+//             let category = new Category(req.body)
+//             product.last_modified_by = req.user.id
+//             await category.save()
+//             if (category.id) {
+//                 const BUCKET_PUBLIC_PATH = process.env.BUCKET_PUBLIC_PATH || config.get('BUCKET_PUBLIC_PATH')
+//                 category.icon = `${BUCKET_PUBLIC_PATH}${category.icon}`
+//                 res.json(category)
+//             }
+//         }
+//     } catch (err) {
+//         console.error(err.message)
+//         res.status(500).send({ "error": MSGS.GENERIC_ERROR })
+//     }
+// })
 
 module.exports = router
