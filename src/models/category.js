aed946c4b7a2f433484579dadf835095144dab10
Lucas Pampolha <lucaspampolhadasilva@gmail.com>
2020-12-31 12:09:40 -0300

    Adição do ícone no model category

--- src/models/category.js
+++ src/models/category.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 const CategorySchema = new mongoose.Schema({
-    name: String,
-    slug: String
+    name : {
+        type : String,
+        required : true
+    },
+    icon: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String
+    },
 }, {autoCreate: true})
 
 module.exports = mongoose.model('category', CategorySchema)
